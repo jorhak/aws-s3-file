57d093beaf8e5687b5c0f5b58dd8b8c55016d539
Pierluigi Vaca Garcia <itfuturovaca@gmail.com>
2022-09-23 15:26:35 -0400

    Primer commit

---
+++ s3.js
@@ -0,0 +1,34 @@
+import {S3Client,PutObjectCommand,ListObjectsCommand} from "@aws-sdk/client-s3"
+
+import {AWS_BUCKET_NAME,AWS_BUCKET_REGION,AWS_PUBLIC_KEY,AWS_SECRET_KEY} from "./config.js"
+
+import fs from "fs"
+
+const client = new S3Client({
+    region: AWS_BUCKET_REGION,
+    credentials: {
+        accessKeyId: AWS_PUBLIC_KEY,
+        secretAccessKey: AWS_SECRET_KEY,
+    }
+})
+
+//Funcion para subir un archibo
+export async function uploadFile(file){
+    const stream = fs.createReadStream(file.tempFilePath)
+    const uploadParams = {
+        Bucket: AWS_BUCKET_NAME,
+        Key: file.name,
+        Body: stream,
+    }
+    const command = new PutObjectCommand(uploadParams)
+    return await client.send(command)
+
+}
+
+export async function getFiles(){
+    const command = new ListObjectsCommand({
+        Bucket: AWS_BUCKET_NAME,
+    })
+    return await client.send(command)
+
+}
